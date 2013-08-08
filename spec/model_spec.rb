d005db1366044db83fb6e998fb1f4abb1bd8b1be
Pol Miro <polmiro@gmail.com>
2013-08-08 14:18:13 -0700

    Fix model specs

--- spec/model_spec.rb
+++ spec/model_spec.rb
@@ -8,35 +8,35 @@ describe TestModel do
 
   let(:model) { TestModel.new(image_url: 'http://filepicker.io/image') }
 
-  describe "#image_url" do
+  describe "#image" do
     context "when no url" do
       it "returns nil" do
         model.image_url = nil
-        model.image_url.should be_nil
+        model.image.should be_nil
       end
     end
 
     context "when called without arguments" do
       it "returns the original url" do
-        model.image_url.should == 'http://filepicker.io/image'
+        model.image.should == 'http://filepicker.io/image'
       end
     end
 
     context "when called with options" do
       it "returns the url with the options" do
-        model.image_url(w: 1, h: 2).should == 'http://filepicker.io/image/convert?w=1&h=2&fit=max'
+        model.image(w: 1, h: 2).should == 'http://filepicker.io/image/convert?w=1&h=2&fit=max'
       end
     end
 
     context "when called with a style" do
       it "returns the url with the dimensions of the style" do
-        model.image_url(:small).should == 'http://filepicker.io/image/convert?w=50&h=100&fit=max'
+        model.image(:small).should == 'http://filepicker.io/image/convert?w=50&h=100&fit=max'
       end
     end
 
     context "when called with a style and options" do
       it "returns the url with the dimensions of the style" do
-        model.image_url(:small, fit: 'crop').should == 'http://filepicker.io/image/convert?fit=crop&w=50&h=100'
+        model.image(:small, fit: 'crop').should == 'http://filepicker.io/image/convert?fit=crop&w=50&h=100'
       end
     end
   end
